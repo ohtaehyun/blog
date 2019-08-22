01303bef316fb8786c3b17569e627ed09b5cc6be
ohtaehyun <othdev95@gmail.com>
2019-08-22 10:29:14 +0900

    adjust basic.css and other page's background img and add get post_list using ajax from json file

---
+++ js/post_list.js
@@ -0,0 +1,32 @@
+const postList = $(".post_list");
+
+function paintPostList(posts) {
+  console.log(posts.post_list);
+  for (let index = 0; index < posts.post_list.length; index++) {
+    const title = posts.post_list[`${index}`].title;
+    const pre_content = posts.post_list[`${index}`].pre_content;
+    const href = posts.post_list[`${index}`].href;
+    postList.append(`<div class="bubble">
+      <div class="bubble-title">
+        <a href=${href}><h1>${title}</h1></a>
+      </div>
+      <div class="divider"></div>
+      <div class="bubble-content">
+        <p>${pre_content}</p>
+      </div>
+    </div>`);
+  }
+}
+
+function init() {
+  // dosomething
+  fetch("../json/post_list.json")
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(json) {
+      paintPostList(json);
+    });
+}
+
+init();

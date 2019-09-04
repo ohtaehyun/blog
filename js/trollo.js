9a34a57bfeb0800d9d1e66c9ff7d9e475cbb1f35
ohtaehyun <othdev95@gmail.com>
2019-09-04 13:25:09 +0900

    make modal

--- js/trollo.js
+++ js/trollo.js
@@ -1,13 +1,84 @@
 const row = document.querySelector(".row");
 const addListBtn = document.querySelector(".add-list-btn");
+const body = document.querySelector("body");
 let bubbles = document.querySelectorAll(".bubble");
 
 const CLASS_BUBBLE = "bubble";
 const CLASS_DISPLAY_NONE = "display-none";
 const CLASS_CARD = "card";
+const CLASS_MODAL_FRAME = "modal-frame";
+const CLASS_CARD_TEXT = "card-text";
+const CLASS_MODAL = "modal";
+const CLASS_EXIT_BUTTON = "exit-button";
+const CLASS_CARD_SUBMIT = "card-submit";
 const DEFAULT_LIST_NAME = "SOMETHING";
 const DEFAULT_CARD_TEXT = "SOMETHING TO DO";
+const DEFAULT_DESC_TEXT = "DESCRIPTION HERE";
 let trolloList = [];
+
+function exitButtonClicked(exitButton, bubbleIdx, selfIdx) {
+  exitButton.addEventListener("click", function() {
+    localStorage.setItem("trolloList", JSON.stringify(trolloList));
+    drawBubbles();
+    body.removeChild(exitButton.parentNode.parentNode);
+  });
+}
+
+function cardTextBlur(textarea, bubbleIdx, selfIdx) {
+  textarea.addEventListener("blur", function() {
+    trolloList[bubbleIdx].cardList[selfIdx] = this.value;
+  });
+}
+
+function cardDescBlur(textarea, bubbleIdx, selfIdx) {
+  textarea.addEventListener("blur", function() {
+    trolloList[bubbleIdx].descList[selfIdx] = this.value;
+  });
+}
+
+function textareaHeightHandle() {
+  this.style.height = "";
+  this.style.height = this.scrollHeight + "px";
+}
+
+function cardClicked(event) {
+  const bubbleIdx = Array.from(row.children).indexOf(
+    this.parentNode.parentNode
+  );
+
+  const selfIdx = Array.from(this.parentNode.children).indexOf(this);
+
+  modalFrame = document.createElement("div");
+  modalFrame.classList.add(CLASS_MODAL_FRAME);
+  modal = document.createElement("div");
+  modal.classList.add(CLASS_MODAL);
+  cardText = document.createElement("textarea");
+  cardText.textContent = this.querySelector(".card-text").textContent;
+  cardText.addEventListener("input", textareaHeightHandle);
+  cardText.classList.add(CLASS_CARD_TEXT);
+  cardTextBlur(cardText, bubbleIdx, selfIdx);
+
+  small = document.createElement("small");
+  small.textContent = "in list " + trolloList[bubbleIdx].name;
+  desText = document.createElement("textarea");
+  desText.textContent = trolloList[bubbleIdx].descList[selfIdx];
+  desText.addEventListener("input", textareaHeightHandle);
+  cardDescBlur(desText, bubbleIdx, selfIdx);
+
+  exitButton = document.createElement("button");
+  exitButton.textContent = "X";
+  exitButton.classList.add(CLASS_EXIT_BUTTON);
+
+  exitButtonClicked(exitButton, bubbleIdx, selfIdx);
+
+  modal.appendChild(exitButton);
+  modal.appendChild(cardText);
+  modal.appendChild(small);
+  modal.appendChild(desText);
+
+  modalFrame.appendChild(modal);
+  body.appendChild(modalFrame);
+}
 
 function bubbleTitleBlur(event) {
   const bubbleIdx = Array.from(row.children).indexOf(
@@ -21,26 +92,27 @@ function dropBtnClick(event) {
   this.parentNode.querySelector(".drops").classList.toggle(CLASS_DISPLAY_NONE);
 }
 
-function cardSubmitClicked() {
-  bubbles = document.querySelectorAll(".bubble");
+function cardSubmitClicked(event) {
   const bubbleIdx = Array.from(row.children).indexOf(
     this.parentNode.parentNode.parentNode
   );
+
   const selfIdx = Array.from(this.parentNode.parentNode.children).indexOf(
     this.parentNode
   );
-  console.log("bidx", bubbleIdx);
-  console.log("sidx", selfIdx);
-  console.log(trolloList);
+
   trolloList[bubbleIdx].cardList[selfIdx] = this.parentNode.querySelector(
     ".card-text"
   ).value;
+
   localStorage.setItem("trolloList", JSON.stringify(trolloList));
-  console.log(trolloList);
+  this.parentNode.addEventListener("click", cardClicked);
+  this.parentNode.innerHTML = `<p class="card-text">${
+    this.parentNode.querySelector(".card-text").value
+  }</p>`;
 }
 
 function addCardClick(event) {
-  const addCardBtns = document.querySelectorAll(".add-card");
   const bubbleIdx = Array.from(row.children).indexOf(
     this.parentNode.parentNode.parentNode
   );
@@ -48,16 +120,27 @@ function addCardClick(event) {
   this.parentNode.classList.add(CLASS_DISPLAY_NONE);
   card = document.createElement("div");
   card.classList.add(CLASS_CARD);
-  card.innerHTML = `<textarea class="card-text" name="" id="" >${DEFAULT_CARD_TEXT}</textarea>
-  <button class="card-submit">save</button>`;
+
+  cardText = document.createElement("textarea");
+  cardText.classList.add(CLASS_CARD_TEXT);
+  cardText.textContent = DEFAULT_CARD_TEXT;
+  cardText.addEventListener("input", textareaHeightHandle);
+  cardText.addEventListener("blur", cardSubmitClicked);
+
+  cardSubmit = document.createElement("button");
+  cardSubmit.classList.add(CLASS_CARD_SUBMIT);
+  cardSubmit.addEventListener("click", cardSubmitClicked);
+  cardSubmit.textContent = "save";
+
+  card.appendChild(cardText);
+  card.appendChild(cardSubmit);
+
   this.parentNode.parentNode.parentNode
     .querySelector(".bubble-content")
     .appendChild(card);
-
-  card
-    .querySelector(".card-submit")
-    .addEventListener("click", cardSubmitClicked);
+  cardText.focus();
   trolloList[bubbleIdx].cardList.push(DEFAULT_CARD_TEXT);
+  trolloList[bubbleIdx].descList.push(DEFAULT_DESC_TEXT);
   localStorage.setItem("trolloList", JSON.stringify(trolloList));
 }
 
@@ -78,9 +161,10 @@ function addListBtnClick(event) {
   bubble
     .querySelector(".bubble-title input")
     .addEventListener("blur", bubbleTitleBlur);
-  obj = {
+  const obj = {
     name: DEFAULT_LIST_NAME,
-    cardList: []
+    cardList: [],
+    descList: []
   };
   trolloList.push(obj);
   bubbles = document.querySelectorAll(".bubble");
@@ -91,6 +175,8 @@ function addListBtnClick(event) {
 }
 
 function drawBubbles() {
+  row.innerHTML = "";
+  trolloList = JSON.parse(localStorage.getItem("trolloList"));
   console.log("trollo.length", trolloList.length);
   trolloList.forEach(element => {
     const bubble = document.createElement("div");
@@ -109,12 +195,9 @@ function drawBubbles() {
     element.cardList.forEach(element2 => {
       card = document.createElement("div");
       card.classList.add(CLASS_CARD);
-      card.innerHTML = `<textarea class="card-text" name="" id="" >${element2}</textarea>
-        <button class="card-submit">save</button>`;
+      card.innerHTML = `<p class="card-text" name="" id="" >${element2}</p>`;
       bubble.querySelector(".bubble-content").appendChild(card);
-      card
-        .querySelector(".card-submit")
-        .addEventListener("click", cardSubmitClicked);
+      card.addEventListener("click", cardClicked);
     });
   });
 }
@@ -123,7 +206,6 @@ function init() {
   addListBtn.addEventListener("click", addListBtnClick);
 
   if (localStorage.getItem("trolloList")) {
-    trolloList = JSON.parse(localStorage.getItem("trolloList"));
     drawBubbles();
   }
 }
